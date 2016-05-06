5573791b6096622580a0c0024d306bb073a06ab6
Marcel Franke <info@marcel-edmund-franke.de>
2016-05-06 16:43:12 +0200

    slash removed (code refactored)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new \FOS\OAuthServerBundle\FOSOAuthServerBundle()
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
