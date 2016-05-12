04567768821bcfb91125890a380e35f278ff9f89
Marcel Franke <info@marcel-edmund-franke.de>
2016-05-12 11:38:28 +0200

    - configuration for dev database added - create dev database script added

---
+++ create-dev-db-with-test-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo chmod 777 -R var/logs var/cache var/sessions
+php bin/console cache:clear --env=dev
+php bin/console doctrine:database:drop -env=dev --force
+php bin/console doctrine:database:create -env=dev
+php bin/console doctrine:schema:update --force --env=dev
+php bin/console doctrine:fixtures:load  --env=dev  --fixtures=tests/AppBundle/DataFixtures/ORM
+php bin/console cache:clear --env=dev
+sudo chmod 777 -R var/logs var/cache var/sessions
