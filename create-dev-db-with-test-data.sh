f6d4a4cf6ac961ca905d38c6eeb8559717cd18d5
Marcel Franke <info@marcel-edmund-franke.de>
2016-05-15 15:12:20 +0200

    bug fix (parameters)

--- create-dev-db-with-test-data.sh
+++ create-dev-db-with-test-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo chmod 777 -R var/logs var/cache var/sessions
 php bin/console cache:clear --env=dev
-php bin/console doctrine:database:drop -env=dev --force --if-exists
-php bin/console doctrine:database:create -env=dev
+php bin/console doctrine:database:drop --env=dev --force --if-exists
+php bin/console doctrine:database:create --env=dev
 php bin/console doctrine:schema:update --force --env=dev
 php bin/console doctrine:fixtures:load  --env=dev  --fixtures=tests/AppBundle/DataFixtures/ORM
 php bin/console cache:clear --env=dev
