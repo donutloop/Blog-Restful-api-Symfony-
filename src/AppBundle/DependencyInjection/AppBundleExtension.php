8a1d81e1ef185ccdf43f6d3d31bd76d4e756febf
Anderson Aguiar <revsoft10@gmail.com>
2016-06-14 22:02:13 -0300

    Fix typo

--- src/AppBundle/DependencyInjection/AppBundleExtension.php
+++ src/AppBundle/DependencyInjection/AppBundleExtension.php
@@ -17,7 +17,7 @@ class AppBundleExtension extends Extension{
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__. '/../Resources/config/'));
-        $loader->load('serivces.yml');
+        $loader->load('services.yml');
     }
-    
+
 }
