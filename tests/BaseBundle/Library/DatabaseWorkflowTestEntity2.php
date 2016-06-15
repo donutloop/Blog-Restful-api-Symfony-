2353862d5def33f8a5d6398d0273f2b6d401481a
Marcel Franke <info@marcel-edmund-franke.de>
2016-06-15 21:46:23 +0200

    BAPI-4 DataBaseWorkFllow improve (new methods & test cases)

---
+++ tests/BaseBundle/Library/DatabaseWorkflowTestEntity2.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Marcel Edmund Franke <info@marcel-edmund-franke.de>
+ */
+
+namespace Tests\BaseBundle\Library;
+
+use BaseBundle\Library\DatabaseWorkflowEntityInterface;
+
+class DatabaseWorkflowTestEntity2 implements DatabaseWorkflowEntityInterface{
+
+    /**
+     * @var
+     */
+    private $identifier;
+
+    /**
+     * @inheritDoc
+     */
+    public function getLiteralType()
+    {
+        return 'DatabaseWorkflowTestEntity';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLiteralName()
+    {
+        return 'DatabaseWorkflowTestEntity';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIdentifier()
+    {
+        return uniqid();
+    }
+}
---
+++ tests/BaseBundle/Unit/Library/DatabaseWorkflowCollectionTest.php
@@ -0,0 +1,79 @@
+<?php
+namespace Tests\BaseBundle\Unit\Library;
+
+use BaseBundle\Library\DatabaseWorkflowCollection;
+use BaseBundle\Library\DatabaseWorkflowEntityInterface;
+use Tests\BaseBundle\Library\DatabaseWorkflowTestEntity;
+use Tests\BaseBundle\Library\DatabaseWorkflowTestEntity2;
+
+
+class DatabaseWorkflowCollectionTest extends \PHPUnit_Framework_TestCase
+{
+
+   public function getEntity(): DatabaseWorkflowEntityInterface{
+      return new DatabaseWorkflowTestEntity();
+   }
+
+   public function getEntity2(): DatabaseWorkflowEntityInterface{
+       return new DatabaseWorkflowTestEntity2();
+   }
+
+   public function testConstructor(){
+       $collection = new DatabaseWorkflowCollection(array($this->getEntity()));
+       static::assertEquals(1, $collection->count());
+   }
+
+    public function testConstructorInvalidArg(){
+        $this->setExpectedException("InvalidArgumentException");
+        $collection = new DatabaseWorkflowCollection(array($this->getEntity(), $this->getEntity2()));
+    }
+
+    public function testAddEntity(){
+       
+       $collection = new DatabaseWorkflowCollection();
+       
+       $collection->addEntity($this->getEntity());
+       $collection->addEntity($this->getEntity());
+
+       static::assertEquals(2, $collection->count());
+   }
+
+    public function testAddEntityInvalidArg() {
+        $this->setExpectedException("InvalidArgumentException");
+
+        $collection = new DatabaseWorkflowCollection();
+
+        $collection->addEntity($this->getEntity());
+        $collection->addEntity($this->getEntity2());
+    }
+
+    public function testSetEntities(){
+
+        $collection = new DatabaseWorkflowCollection();
+
+        $collection->setEntities(array($this->getEntity(), $this->getEntity()));
+
+        static::assertEquals(2, $collection->count());
+    }
+
+    public function testSetEntitiesInvalidArg() {
+        $this->setExpectedException("InvalidArgumentException");
+
+        $collection = new DatabaseWorkflowCollection();
+
+        $collection->setEntities(array($this->getEntity(), $this->getEntity2()));
+    }
+
+   public function testRemoveEntity(){
+
+       $entity = $this->getEntity();
+
+       $id = $entity->getIdentifier();
+
+       $collection = new DatabaseWorkflowCollection(array($entity));
+
+       $collection->removeEntity($id);
+
+       static::assertEquals(1, $collection->count());
+   }
+}
